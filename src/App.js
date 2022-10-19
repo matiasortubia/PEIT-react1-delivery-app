960c661647e7b4b77046794c7ab1691141706128
Guillermo Fernandez <uhgl33@gmail.com>
2022-10-19 01:26:30 -0300

    header: title, cartIcon & restaurants avaible

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Header from './components/Header';
+import Header from './components/header/Header';
 
 function App() {
   return (
