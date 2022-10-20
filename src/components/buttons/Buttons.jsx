f078fc11ad26380a87f410d243150e4defb92a0f
Gabi Iezzi <geiezzi90@gmail.com>
2022-10-19 21:47:09 -0300

    add structure: components navbar,buttons

---
+++ src/components/buttons/Buttons.jsx
@@ -0,0 +1,9 @@
+
+
+export const Buttons = ({name}) => {
+
+
+  return (
+    <button>{name}</button>
+  )
+}
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Buttons } from "../buttons/Buttons"
+export const Navbar = () => {
+  return (
+    <>
+        <Buttons name="Home"  />
+        <Buttons name="Profile"  />
+        <Buttons name="Favs"  />
+        <Buttons name="Notifications"  />
+    </>
+  )
+}
+
