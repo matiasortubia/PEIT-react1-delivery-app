c549546f1a4675aea4576dd267e44b3d3a0e022e
matiasortubia <matiortubia@gmail.com>
2022-11-21 18:44:23 -0300

    code cleaning && merge with development

--- src/components/addressForm/AddressForm.jsx
+++ src/components/addressForm/AddressForm.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
 import styles from './addressForm.module.css';
 
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const AddressForm = ({handleInfoSubmit}) => {
-    const [addressText, setAddressText] = useState('');
-    const [addressNumber, setAddressNumber] = useState('');
-    const [apartmentText, setApartmentText] = useState('');
-    const [extraInfoText, setExtraInfotext] = useState('');
-    const [isValidInput, setIsValidInput] = useState({"street": true, "addressNumber": true, "apartmentText": true, "extraInfo": true});
-
     const formik = useFormik({
         initialValues: {
             addressStreet: '',
@@ -25,7 +18,6 @@ const AddressForm = ({handleInfoSubmit}) => {
             addressStreet: Yup.string()
                 .max(30, "Street name is too long.")
                 .required("Field required.")
-                //.matches(/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/, "Invalid input."),
                 .matches(/^[-@'À-ÖØ-öø-ÿ.\/#&\w\s]*$/, "Invalid input."),
             addressNumber: Yup.number()
                 .min(1, "Invalid input.")
@@ -45,20 +37,6 @@ const AddressForm = ({handleInfoSubmit}) => {
                                                       values.extraInfo);
         }
     });
-
-    /* Input validations */
-    const validateOnlyLetters = input => {
-        return input !== '' && (/^[(A-Za-zÀ-ÖØ-öø-ÿ)|.|']*$/.test(input));
-    };
-    /* **************** */
-
-    const infoSubmit = e => {
-        e.preventDefault();
-        const addressTrimmed = addressText.trim();
-        let isValidStreet = validateOnlyLetters(addressTrimmed);
-        setIsValidInput({street: isValidStreet});
-        isValidStreet && handleInfoSubmit(addressTrimmed, addressNumber, apartmentText, extraInfoText);
-    };
 
     return (
         <form onSubmit={ formik.handleSubmit } className={ styles.addressForm }>
@@ -128,73 +106,8 @@ const AddressForm = ({handleInfoSubmit}) => {
             </div>
             <button className={styles.formSubmit}
                     type='submit'>Save</button>
-
-
         </form>
     );
 }
 
 export { AddressForm };
-/*
-    return (
-        <form className={ styles.addressForm } onSubmit={infoSubmit}>
-            <div className={ styles.inputWrapper } >
-                <label htmlFor="addressInput">Street</label>
-                <input className={ `${ styles.addressFormInput } ${ isValidInput.street ? "" : styles.invalidInput }` }
-                       type="text" 
-                       required
-                       placeholder="Example St." 
-                       id="addressInput"
-                       name="address"
-                       autoComplete="off"
-                       spellCheck="false" 
-                       value={addressText} 
-                       maxLength="25"
-                       onChange={e => setAddressText(e.target.value)} />
-            </div>
-            <div className={ styles.inputWrapper } >
-                <label htmlFor="addressNumberInput">Address Number</label>
-                <input type="number" 
-                       required
-                       placeholder="1234" 
-                       id="addressNumberInput"
-                       name="addressNumber"
-                       autoComplete="off"
-                       min="1"
-                       max="9999"
-                       value={addressNumber} 
-                       onChange={e => setAddressNumber(e.target.value)} />
-            </div>
-            <div className={ styles.inputWrapper } >
-                <label htmlFor="apartmentInput">Floor/Apartment</label>
-                <input type="text" 
-                       placeholder="1st floor, ap. A" 
-                       id="apartmentInput"
-                       name="aparment" 
-                       autoComplete="off"
-                       spellCheck="false"
-                       maxLength="25"
-                       value={apartmentText}
-                       onChange={e => setApartmentText(e.target.value)} />
-            </div>
-            <div className={ styles.inputWrapper }>
-                <label htmlFor="extraInfoInput">Extra information</label>
-                <textarea className={styles.addressTextArea}
-                       placeholder="Ex: Flat on a corner"
-                       id="extraInfoInput"
-                       name="extraInfo" 
-                       autoComplete="off"
-                       spellCheck="false"
-                       maxLength="255"
-                       value={extraInfoText} 
-                       onChange={e=> setExtraInfotext(e.target.value)} />
-                
-            </div>
-            <button className={styles.formSubmit}
-                    type="submit"
-                    onClick={e => e.preventDefault}>Save</button>
-        </form>
-    );
-};
-
-export { AddressForm }; */
